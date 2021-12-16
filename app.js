d593e0619d73e3a24f17b6829eb4b9de96c42d6d
Bus42 <gbrewton1980@gmail.com>
2021-12-15 19:00:38 -0700

    stubs out server and GET /users

---
+++ app.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+var app = express();
+
+require('dotenv').config();
+const port = process.env.PORT || 3000;
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+})
+
+module.exports = app;
