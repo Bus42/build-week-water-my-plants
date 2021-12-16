dd4041f3585766a8c72fbd5bf5173ee4f33e7273
Bus42 <gbrewton1980@gmail.com>
2021-12-15 20:58:36 -0700

    can register user

--- models/usersModel.js
+++ models/usersModel.js
@@ -1,9 +1,31 @@
 const db = require('../config/');
 
 async function getUsers() {
-    return await db('users').select();
+    const users = await db('users').select('id', 'username', 'phoneNumber');
+}
+
+async function addUser(user) {
+    const [id] = await db('users').insert(user);
+    const users = await db('users').where({ id });
+    const returnedUser = users[0];
+    delete returnedUser.password;
+    return returnedUser;
+}
+
+async function getUserBy(filter) {
+    const users = await db('users').where(filter);
+    return users[0];
+}
+
+async function getUserById(id) {
+    const user = await getUserBy({ id });
+    delete user.password;
+    return user;
 }
 
 module.exports = {
-    getUsers
-}
+    getUsers,
+    addUser,
+    getUserBy,
+    getUserById
+};
--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,21 @@
 var express = require('express');
 var router = express.Router();
 const usersDb = require('../models/usersModel');
+const bcrypt = require('bcrypt');
 
 router.get('/', function (req, res, next) {
   usersDb.getUsers().then(users => res.status(200).json(users))
     .catch(err => res.status(500).json({ error: err, message: "error getting users" }));
 });
+
+router.post('/', function (req, res, next) {
+  const userInfo = {
+    username: req.body.username,
+    password: bcrypt.hashSync(req.body.password, 8),
+    phoneNumber: req.body.phoneNumber
+  }
+  usersDb.addUser(userInfo).then(user => res.status(200).json(user))
+    .catch(err => res.status(500).json({ error: err, message: "error adding user" }));
+})
 
 module.exports = router;
