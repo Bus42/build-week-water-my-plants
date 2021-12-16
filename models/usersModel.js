6d3b9d74dead159622a15a53568f4fd9c1405ddc
Bus42 <gbrewton1980@gmail.com>
2021-12-15 21:30:25 -0700

    user can log in

--- models/usersModel.js
+++ models/usersModel.js
@@ -1,11 +1,20 @@
+require('dotenv').config();
 const db = require('../config/');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
 
 async function getUsers() {
     const users = await db('users').select('id', 'username', 'phoneNumber');
+    return users;
 }
 
 async function addUser(user) {
-    const [id] = await db('users').insert(user);
+    const hashedUser = {
+        username: user.username,
+        password: bcrypt.hashSync(user.password, 8),
+        phoneNumber: user.phoneNumber
+    }
+    const [id] = await db('users').insert(hashedUser);
     const users = await db('users').where({ id });
     const returnedUser = users[0];
     delete returnedUser.password;
@@ -18,14 +27,28 @@ async function getUserBy(filter) {
 }
 
 async function getUserById(id) {
-    const user = await getUserBy({ id });
-    delete user.password;
-    return user;
+    const users = await db('users').where({ id });
+    const returnedUser = users[0];
+    delete returnedUser.password;
+    return returnedUser;
+}
+
+async function loginUser(user) {
+    const username = user.username;
+    const users = await db('users').where({ username });
+    const returnedUser = users[0];
+    const isValid = bcrypt.compareSync(user.password, returnedUser.password);
+    if (isValid) {
+        const token = jwt.sign({ username }, process.env.JWT_SECRET, { expiresIn: '1h' });
+        return token;
+    }
+    return null;
 }
 
 module.exports = {
     getUsers,
     addUser,
     getUserBy,
-    getUserById
+    getUserById,
+    loginUser
 };
--- routes/users.js
+++ routes/users.js
@@ -3,19 +3,24 @@ var router = express.Router();
 const usersDb = require('../models/usersModel');
 const bcrypt = require('bcrypt');
 
-router.get('/', function (req, res, next) {
+router.get('/', function (req, res) {
   usersDb.getUsers().then(users => res.status(200).json(users))
     .catch(err => res.status(500).json({ error: err, message: "error getting users" }));
 });
 
-router.post('/', function (req, res, next) {
-  const userInfo = {
-    username: req.body.username,
-    password: bcrypt.hashSync(req.body.password, 8),
-    phoneNumber: req.body.phoneNumber
-  }
-  usersDb.addUser(userInfo).then(user => res.status(200).json(user))
+router.post('/', function (req, res) {
+  usersDb.addUser(req.body).then(user => res.status(200).json(user))
     .catch(err => res.status(500).json({ error: err, message: "error adding user" }));
+})
+
+router.post('/login', async function (req, res) {
+  const token = await usersDb.loginUser(req.body);
+  if (token) {
+    res.status(200).json(token);
+  }
+  else {
+    res.status(401).json({ message: "invalid credentials" });
+  }
 })
 
 module.exports = router;
