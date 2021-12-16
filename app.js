1578f023da8f21b468e4d54cd779eb7c253e5d68
Bus42 <gbrewton1980@gmail.com>
2021-12-15 23:27:51 -0700

    build(API): Endpoints
    Stubs out:
    Get Users
    Get Plants
    Register
    Login

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const logger = require('morgan');
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 const plantsRouter = require('./routes/plants');
+const cors = require('cors');
 
 const app = express();
 
@@ -16,6 +17,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(cors());
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
