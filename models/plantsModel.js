098e6f0701c26cfdc5be0d4d9828c16967e7fcdc
Bus42 <gbrewton1980@gmail.com>
2021-12-19 11:16:38 -0700

    CRUD operations for plants endpoints

--- models/plantsModel.js
+++ models/plantsModel.js
@@ -1,9 +1,50 @@
 const db = require('../config/');
+const colors = require('colors');
 
 async function getPlants() {
-    return await db('plants').select();
+    return await db('plants');
+}
+
+async function getPlantBy(filter) {
+    return await db('plants').where(filter);
+}
+
+async function getPlantById(id) {
+    return await db('plants').where({ id });
+}
+
+async function addPlant(plant) {
+    return await db('plants').insert(plant);
+}
+
+async function updatePlant({ id, plant }) {
+    const success = await db('plants').where({ id }).update(plant);
+    if (success) {
+        console.log(colors.bgYellow.black.bold(`Plant ${id} updated`));
+        return await getPlantById(id);
+    } else {
+        console.log(colors.bgRed.black.bold(`Internal error updating Plant ${id}`));
+        return null;
+    }
+}
+
+async function deletePlant(id) {
+    console.log(id)
+    const success = await db('plants').where({ id }).del();
+    if (success) {
+        console.log(colors.bgYellow.black.bold(`Plant ${id} deleted`));
+        return true;
+    } else {
+        console.log(colors.bgRed.black.bold(`Internal error deleting Plant ${id}`));
+        return false;
+    }
 }
 
 module.exports = {
-    getPlants
-}
+    getPlants,
+    getPlantBy,
+    getPlantById,
+    addPlant,
+    updatePlant,
+    deletePlant
+};
--- routes/plants.js
+++ routes/plants.js
@@ -7,4 +7,26 @@ router.get('/', function (req, res, next) {
         .catch(err => res.status(500).json({ error: err, message: "error getting plants" }));
 });
 
+router.get('/:id', function (req, res) {
+    plantsDb.getPlantById(req.params.id).then(plant => res.status(200).json(plant))
+        .catch(err => res.status(500).json({ error: err, message: "error getting plant" }));
+});
+
+router.post("/", function (req, res) {
+    plantsDb.addPlant(req.body).then(plant => res.status(200).json(plant))
+        .catch(err => res.status(500).json({ error: err, message: "error adding plant" }));
+});
+
+router.put("/:id", function (req, res) {
+    const id = req.params.id;
+    const plant = req.body;
+    plantsDb.updatePlant({ id, plant }).then(updatedPlant => res.status(200).json(updatedPlant))
+});
+
+router.delete("/:id", function (req, res) {
+    const id = req.params.id;
+    plantsDb.deletePlant(id).then(() => res.status(200).json(`user ${id} deleted`))
+        .catch(err => res.status(500).json({ error: err, message: "error deleting plant" }));
+});
+
 module.exports = router;
